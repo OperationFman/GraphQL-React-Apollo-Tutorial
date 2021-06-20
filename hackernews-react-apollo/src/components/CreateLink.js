4a446e252a01b65d8a3e5102e2db574d9b36b16b
Franklin Moon <franklin.m.moon@gmail.com>
2021-06-20 19:02:43 +1000

    Update CreateLink.js

---
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const CreateLink = () => {
+    const [formState, setFormState] = useState({
+        description: '',
+        url: ''
+    });
+
+    return (
+        <div>
+            <form onSubmit={(e) => {e.preventDefault();}}>
+            <div className="flex flex-column mt3">
+                <input
+                    classname="mb2"
+                    value={formState.description}
+                    onChange={(e) => setFormState({...formState, description: e.target.value})}
+                    type="text"
+                    placeholder="A description for the link"
+                />
+                <input
+                    className="mb2"
+                    value={formState.url}
+                    onChange={(e) => setFormState({...formState, url: e.target.value })}
+                    type="text"
+                    placeholder="The URL for the Link"
+                />
+            </div>
+            <button type="submit">Submit"</button>
+            </form>
+        </div>
+    )
+}
